ad293035111176b0b735a77598480cfcc368fc99
Carolina Nonato <csnonato@gmail.com>
2022-03-21 22:53:05 -0400

    writing recipes logs

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var axios = require("axios").default;
-
-
+const writeRecipe = require("./writeRecipe");
 const recipe = process.argv[2]
 
 
@@ -17,7 +16,10 @@ var options = {
 axios.request(options).then(function (response) {
     const data = response.data;
     const info = data.data
+    writeRecipe(`Recipe: ${info[0].name}\n Ingredients: ${info[0].ingredients}\n Instructions: ${info[0].instructions}\n\n\n   `);
     console.log(info)
+
 }).catch(function (error) {
     console.error(error);
 });
+
---
+++ writeRecipe.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const writeToLog = (info) => {
+  try {
+    const path = "log.txt";
+    const data = fs.readFileSync(path, { encoding: 'utf8', flag: 'r' });
+    fs.writeFileSync(path, `${data}${info}\n`);
+  } catch (err) {
+    console.log(err);
+
+  }
+}
+
+module.exports = writeToLog;
